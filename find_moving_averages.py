128d83809b0e40a14415ab4eefbe5c87e1e1f24a
Darryl Smith <ds185216@gmail.com>
2020-05-22 19:14:43 +1000

    Add files via upload

--- find_moving_averages.py
+++ find_moving_averages.py
@@ -50,40 +50,45 @@ def find_averages(db_buy, db_sell):
 			inc = min(increments)
 			for reverse in [True, False]:
 				for test_ma in range(2, 72):
-					for diff in range(1, 60):
-						cash = 1000.00
-						crypto = 0
-						highest_amount = 0
-						shift = 0
-						for i in range(len(buy_prices)):
-							today = False
-							if float(sell_prices[i]) > highest_amount:
-								highest_amount = float(sell_prices[i])
-							if crypto > 0:
-								if float(sell_prices[i]) < highest_amount - (diff * inc) and cash == 0:
-									#test sell
-									cash = cash + round(crypto * float(sell_prices[i]))
-									crypto= 0
-									today = True
-									highest_amount = 0
-									shift +=1
-							if i >= test_ma+int(math.sqrt(test_ma)) and today != True and cash > 0:
-								average = averages[calc](test_ma, buy_prices[(i-(test_ma+int(math.sqrt(test_ma)))):i])
-								if average < float(buy_prices[i]) or reverse == True and average > float(buy_prices[i]) and crypto == 0:
-									#test buy
-									crypto = crypto + (round(cash) / float(buy_prices[i]))
-									cash = 0
-									highest_amount = float(buy_prices[i])
-									shift +=1
-						if crypto != 0:
-							cash = cash + round(crypto * float(sell_prices[i]))
-						if cash > 1000 and shift > len(db_buy)/288:
-							result.append({'name' : averages_names[calc],
-								'test_ma' : test_ma,
-								'name' : averages_names[calc],
-								'CUR' : CUR,
-								'reverse' : reverse,
-								'differential' : diff*inc})
+					for floor_diff in range(1, 60):
+						for roof_diff in range(1, 60):
+							cash = 1000.00
+							crypto = 0
+							highest_amount = 0
+							buy_amount = 0
+							shift = 0
+							for i in range(len(buy_prices)):
+								today = False
+								if float(sell_prices[i]) > highest_amount:
+									highest_amount = float(sell_prices[i])
+								if crypto > 0:
+									if (float(sell_prices[i]) < highest_amount - (floor_diff * inc) or float(sell_prices[i]) >= ((roof_diff * inc) + buy_amount)) and cash == 0:
+										#test sell
+										cash = cash + round(crypto * float(sell_prices[i]))
+										crypto= 0
+										today = True
+										highest_amount = 0
+										shift +=1
+								if i >= test_ma+int(math.sqrt(test_ma)) and today != True:
+									average = averages[calc](test_ma, buy_prices[(i-(test_ma+int(math.sqrt(test_ma)))):i])
+									if average < float(buy_prices[i]) or reverse == True and average > float(buy_prices[i]) and crypto == 0:
+										#test buy
+										crypto = crypto + (round(cash) / float(buy_prices[i]))
+										cash = 0
+										highest_amount = float(buy_prices[i])
+										buy_amount = float(buy_prices[i])
+										shift +=1
+							if crypto != 0:
+								cash = cash + round(crypto * float(sell_prices[i]))
+							if cash > 1000 and shift > len(db_buy)/288:
+								result.append({'name' : averages_names[calc],
+									'test_ma' : test_ma,
+									'name' : averages_names[calc],
+									'CUR' : CUR,
+									'reverse' : reverse,
+									'floor_differential' : floor_diff*inc,
+									'roof_differential' : roof_diff*inc
+									})
 	print (len(result), 'profitable formulas found')
 
 
@@ -94,31 +99,34 @@ def test_averages(db_buy, db_sell, data):
 		MA = line['name']
 		CUR = line['CUR']
 		reverse = line['reverse']
-		diff = line['differential']
+		floor_diff = line['floor_differential']
+		roof_diff = line['roof_differential']
 
 		buy_prices = [y for y in db_buy[CUR]]
 		sell_prices = [y for y in db_sell[CUR]]
 		cash = 1000.00
 		crypto = 0
 		highest_amount = 0
+		buy_amount = 0
 		for i in range(len(buy_prices)):
 			today = False
 			if float(sell_prices[i]) > highest_amount:
 				highest_amount = float(sell_prices[i])
 			if crypto > 0:
-				if float(sell_prices[i]) < highest_amount - (diff) and cash == 0:
+				if (float(sell_prices[i]) < highest_amount - (floor_diff) or float(sell_prices[i]) >= (roof_diff + buy_amount)) and cash == 0:
 					#test sell
 					cash = cash + round(crypto * float(sell_prices[i]))
 					crypto= 0
 					today = True
 					highest_amount = 0
-			if i >= test_ma+int(math.sqrt(test_ma)) and today != True and cash > 0:
+			if i >= test_ma+int(math.sqrt(test_ma)) and today != True:
 				average = averages_dict[MA](test_ma, buy_prices[(i-(test_ma+int(math.sqrt(test_ma)))):i])
 				if average < float(buy_prices[i]) or reverse == True and average > float(buy_prices[i]) and crypto == 0:
 					#test buy
 					crypto = crypto + (round(cash) / float(buy_prices[i]))
 					cash = 0
 					highest_amount = float(buy_prices[i])
+					buy_amount = float(buy_prices[i])
 		if crypto != 0:
 			cash = cash + round(crypto * float(sell_prices[i]))
 		if cash > overall_cash:
@@ -127,7 +135,8 @@ def test_averages(db_buy, db_sell, data):
 			overall_cur = CUR
 			overall_moving_average = test_ma
 			overall_reverse = reverse
-			overall_difference = diff
+			overall_floor_difference = floor_diff
+			overall_roof_difference = roof_diff
 	print (overall_cash, overall_formula, overall_moving_average, overall_cur, overall_reverse, overall_difference)
 	if overall_cash > 1000:
 		with open('moving-averages-5min', 'wb') as handle:
@@ -135,7 +144,8 @@ def test_averages(db_buy, db_sell, data):
 			pickle.dump(overall_cur, handle)
 			pickle.dump(overall_moving_average, handle)
 			pickle.dump(overall_reverse, handle)
-			pickle.dump(overall_difference, handle)
+			pickle.dump(overall_floor_difference, handle)
+			pickle.dump(overall_roof_difference, handle)
 	else:
 		print ('Not profitable, no data written')
 
